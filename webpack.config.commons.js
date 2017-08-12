22248835c7a8f9f6d254b5435576c4498662ba1b
Christian Grøngaard <christianhg@gmail.com>
2017-08-12 18:07:13 +0200

    Add example using CommonsChunkPlugin

---
+++ webpack.config.commons.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'dist-commons'),
+    port: '8000'
+  },
+  entry: {
+    core: './src/core/index.ts',
+    add: './src/plugins/add/index.ts',
+    subtract: './src/plugins/subtract/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist-commons'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'awesome-typescript-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'core',
+      filename: 'core.js'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.ts']
+  }
+}
