e61bd341a70caebb3de069e10d6564d9991162b1
Christian Hamburger Grøngaard <christianhg@gmail.com>
2017-08-16 12:43:20 +0200

    Make foo prettier

--- src/plugins/foo/index.ts
+++ src/plugins/foo/index.ts
@@ -1,6 +1,6 @@
 import { add } from '../add'
 import { subtract } from '../subtract'
-import { compute } from '../../core';
+import { compute } from '../../core'
 
 console.log(compute(2, add(3), subtract(5)))
 
